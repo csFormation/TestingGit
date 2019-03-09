7e369034bad64af4233a8fa055071a913f1de85e
Miguel <artisanade@gmail.com>
2019-03-09 23:21:21 +0100

    Ref 02#: Modificación de main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-    int i = 2;		
+    int i = 2;
+
+    int j = atoi(argv[1]);
+
     printf("\n-------------Probando la función cuadrado-------------------\n");
-    printf("\nEl cuadrado de %d es %d\n", i, cuadrado(&i)); 
+    printf("\nEl cuadrado de %d es %d\n", j, cuadrado(&j)); 
     
     return 0;
 }
